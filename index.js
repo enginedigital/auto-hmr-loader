1e7e43c43a7adee7aa809a8cad6d55ee923645d1
James Doyle <james2doyle@gmail.com>
2020-01-16 13:00:48 -0800

    Removed: double code and lack of indent

--- index.js
+++ index.js
@@ -25,13 +25,7 @@ module.exports = function(source) {
 
   validationOptions(schema, options, {name: 'Engine Digital HMR Loader'});
 
-  if (options.cache === false) {
-    this.cacheable(false);
-  }
-
-  if (options.cache === false) {
-    this.cacheable(false);
-  }
+  this.cacheable(!!options.cache);
 
   if (process.env.NODE_ENV === 'production' || !this.hot) {
     callback(null, source);
@@ -47,61 +41,62 @@ module.exports = function(source) {
       .map(val => `'${val}'`);
 
     const tmpSource = `${source}
-       const walk = function walk(root, call) {
-        call(root);
-        if (root.shadowRoot) {
-          walk(root.shadowRoot, call);
+/* hot reload */
+const walk = function walk(root, call) {
+  call(root);
+  if (root.shadowRoot) {
+    walk(root.shadowRoot, call);
+  }
+
+  Array.from(root.children).forEach((child) => {
+    walk(child, call);
+  });
+}
+
+const hmr = function hmr() {
+  if (!module.hot) {
+    return;
+  }
+
+  module.hot.accept([${importPaths.join(', ')}], function (deps, ...args) {
+    const comp = __webpack_require__(deps[0]);
+    let Ctor = comp.default;
+
+    if (typeof Ctor === 'undefined' || typeof Ctor.is === 'undefined') {
+      if (module !== void 0 && module.__proto__ !== void 0 && module.__proto__.exports !== void 0 && module.__proto__.exports.default !== void 0)
+      Ctor = module.__proto__.exports.default;
+    }
+    if (typeof Ctor === 'undefined' || typeof Ctor.is === 'undefined') {
+      module.hot.decline([${importPaths.join(', ')}]);
+      return;
+    }
+    walk(document.body, async (node) => {
+      if (node.localName === Ctor.is) {
+        Ctor.observedAttributes;
+        const descriptorsS = Object.getOwnPropertyDescriptors(Ctor);
+        const descriptorsI = Object.getOwnPropertyDescriptors(Ctor.prototype);
+
+        for (const name in descriptorsS) {
+          if (name !== 'length' && name !== 'name' && name !== 'prototype') {
+            Object.defineProperty(node.constructor, name, descriptorsS[name]);
+          }
         }
 
-        Array.from(root.children).forEach((child) => {
-          walk(child, call);
-        });
-      }
-
-      const hmr = function hmr() {
-        if (!module.hot) {
-          return;
+        for (const name in descriptorsI) {
+          Object.defineProperty(node, name, descriptorsI[name]);
         }
 
-        module.hot.accept([${importPaths.join(', ')}], function (deps, ...args) {
-          const comp = __webpack_require__(deps[0]);
-          let Ctor = comp.default;
-
-          if (typeof Ctor === 'undefined' || typeof Ctor.is === 'undefined') {
-            if (module !== void 0 && module.__proto__ !== void 0 && module.__proto__.exports !== void 0 && module.__proto__.exports.default !== void 0)
-            Ctor = module.__proto__.exports.default;
-          }
-          if (typeof Ctor === 'undefined' || typeof Ctor.is === 'undefined') {
-            module.hot.decline([${importPaths.join(', ')}]);
-            return;
-          }
-          walk(document.body, async (node) => {
-            if (node.localName === Ctor.is) {
-              Ctor.observedAttributes;
-              const descriptorsS = Object.getOwnPropertyDescriptors(Ctor);
-              const descriptorsI = Object.getOwnPropertyDescriptors(Ctor.prototype);
-
-              for (const name in descriptorsS) {
-                if (name !== 'length' && name !== 'name' && name !== 'prototype') {
-                  Object.defineProperty(node.constructor, name, descriptorsS[name]);
-                }
-              }
-
-              for (const name in descriptorsI) {
-                Object.defineProperty(node, name, descriptorsI[name]);
-              }
-
-              if (node.connectedCallback) {
-                node.connectedCallback();
-                node.shouldUpdate(Ctor._classProperties);
-                node.requestUpdate();
-              }
-            }
-          });
-        });
+        if (node.connectedCallback) {
+          node.connectedCallback();
+          node.shouldUpdate(Ctor._classProperties);
+          node.requestUpdate();
+        }
       }
+    });
+  });
+}
 
-      hmr();
+hmr();
     `;
 
     source = tmpSource;
